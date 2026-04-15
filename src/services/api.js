0af09b7bf92714c31318afaf8b4cdfe79185a8c2
Dexter722005 <shreshthpanda7@gmail.com>
2026-04-15 12:56:26 +0530

    fix correct backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://spendora-production-1228.up.railway.app/api"
+  baseURL:"https://spendora-production-1228.up.railway.app/"
 });
 
 API.interceptors.request.use((req) => {
